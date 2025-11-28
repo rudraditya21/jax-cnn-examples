e99d56980d2d0a2c507263df411e77f54477f9c0
Rudraditya Thakur <rudraditya.thakur21@gmail.com>
2025-11-28 16:53:01 +0530

    updated: datasets module

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,8 +1,12 @@
-from . import cifar10, cifar100
+from . import cifar10, cifar100, emnist, fashion_mnist, kmnist, mnist
 from .registry import DatasetRegistry
 
 __all__ = [
     "DatasetRegistry",
     "cifar10",
     "cifar100",
+    "emnist",
+    "fashion_mnist",
+    "kmnist",
+    "mnist",
 ]
